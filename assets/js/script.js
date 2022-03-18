c75b32bd9a194e76a7289dc8b6b96ba963eb6dc2
Meyaal <alsuhaili_32@hotmail.com>
2022-03-18 09:14:57 +0000

    add code to class and id to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
+
+/** 
+ * add eventlistner to all the buttons
+ */
+
+for (let button of buttons) {
+    button.addEventListener("click", function(){
+    let playerChoise = this.getAttribute("data-choice");
+    playerGame = (playerChoise);
+});
+
+}
+
+function playerGame (playerChoise) {
+    playerImage.src = `assets/images/${choices[playerChoise]}.png`;
+    playerImage.alt = choices[playerChoise];
+}  
+
+let computerChoise = Math.floor(Math.random() * 3);
+computerImage.src = `assets/images/${choices[computerChoise]}.png`;
+playerImage.alt = choices[computerChoise];
