3096dd21aca193cf31fbe5fee92e2c29f98f5bcc
Meyaal <alsuhaili_32@hotmail.com>
2022-05-22 08:04:32 +0000

    add style to divs

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Declare constants for DOM elements
+const playerScore_span = getElementById("player-score");
+const computerScore_span= getElementById("computer-score");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+let playerScore = 0;
+let computerScore = 0;
