debac45b32c21526d9dfe4bf7f8a4e50c285e4d6
Meyaal <alsuhaili_32@hotmail.com>
2022-05-25 08:22:54 +0000

    add function for win,lose and draw in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,91 @@
 // Declare constants for DOM elements
-const playerScoredisplay = document.getElementById("player-score");
-const computerScoredisplay = document.getElementById("computer-score");
-const resultDisplay = document.getElementById('result')
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const result_div = document.querySelector("#result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
-const win = document.getElementById("player-win");
-
-
 let playerScore = 0;
 let computerScore = 0;
-let result
-let computerChoice
-let playerChoice
+const resetButton = document.getElementById("reset-button");
 
-function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random () * 3)
-
-    switch (randomNumber) {
-   case 0:
-      computerChoice = 'paper';
-      break;
-   case 1:
-      computerChoice = 'scissors';
-      break;
-   case 2:
-      computerChoice = 'rock';
-} 
+/**
+ * Lists the choices
+ * and generates the computer choice by using a random number from the array of choices
+ */  
+ function getComputerChoice() {
+   const choices = ['rock', 'paper', 'scissors'];
+   const randomNumber =Math.floor(Math.random()*choices.length);
+   return choices[randomNumber];
 }
 
-(generateComputerChoice());
+// Gets computer choice
+(getComputerChoice());
 
-// function to decide who wins the game
+/**
+ * Finds the player score and increments it
+ * inserts the result string into the result section
+ */
 
-function decideWinner(playerChoice, computerChoice) {
-   if(playerChoice === "paper" && computerChoice === "paper") {
-      result = "Its a tie!"
-    } else if (playerChoice === "rock" && computerChoice === "paper") 
-      result = "player win!"
-     } if(playerChoice === "rock" && computerChoice === "scissors") 
-      result = "you lost!"
-   
-   
-   
-
-console.log(decideWinner);
-
-   function main () {
-    rock_div.addEventListener ("click", () => decideWinner("rock"));
-
-    paper_div.addEventListener("click", () => decideWinner("paper"));
-    
-    scissors_div.addEventListener("click", () => decideWinner("scissors"));
-
+function win(playerChoice, computerChoice) {
+   playerScore++;
+   playerScore_span.innerHTML = playerScore;
+   computerScore_span.innerHTML = computerScore;
+   document.getElementById(playerChoice).classList.add('winner');
 }
+
+/**
+ * Finds the computer score and increments it
+ * inserts the result string into the result section
+*/
+
+function lose(playerChoice, computerChoice) {
+   computerScore++;
+   playerScore_span.innerHTML = playerScore;
+   computerScore_span.innerHTML = computerScore;
+   document.getElementById(playerChoice).classList.add('loser');
+}  
+
+// Maintains the score and displays a string in the result
+function tie(playerChoice, computerChoice) {
+   playerScore_span.innerHTML = playerScore;
+   computerScore_span.innerHTML = computerScore;
+   document.getElementById(playerChoice).classList.add('tie!');
+}
+
+
+
+function game(playerChoice) {
+   const computerChoice = getComputerChoice();
+   switch(playerChoice + computerChoice) {
+       case "rockscissors":
+       case "paperrock":
+       case "scissorspaper":
+           win(playerChoice, computerChoice);
+           break;
+       case "rockpaper":
+       case "paperscissors":
+       case "scissorsrock":
+           lose(playerChoice, computerChoice);
+           break;
+       case "paperpaper":
+       case "scissorsscissors":
+       case "rockrock":
+           tie(playerChoice, computerChoice);
+           break;
+         }
+      };
+   
+   game();
+
+// Add event listeners to all icons
+function main() {
+   rock_div.addEventListener('click', () => game("rock"));
+
+   paper_div.addEventListener('click', () => game("paper"));
+
+   scissors_div.addEventListener('click', () => game("scissors"));
+};
+
 main();
 
